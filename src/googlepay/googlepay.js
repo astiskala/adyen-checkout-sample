bbc83e83b8151fa73e61f23fbb18441a5cb7198f
Adam Stiskala <adam.stiskala@adyen.com>
2020-05-06 11:32:40 +1000

    Fix Google Pay

--- src/googlepay/googlepay.js
+++ src/googlepay/googlepay.js
@@ -1,29 +1,53 @@
-// 1. Create an instance of AdyenCheckout
-const checkout = new AdyenCheckout({});
+const getConfig = async () => {
+    let config = { googlePayConfig: { configuration: { merchantName: 'Adyen Test' }, amount: {} } };
+    config.locale = await httpGet('env', 'SHOPPER_LOCALE');
+    config.environment = await httpGet('env', 'ENVIRONMENT');
 
-// 2. Create and mount the Component
-const googlepay = checkout
-    .create('paywithgoogle', {
-        showPayButton: true,
-        environment: 'TEST', // Google Pay environment
-        configuration: {
-            gatewayMerchantId: 'TestMerchantCheckout', // name of your Adyen Merchant account
-            merchantName: 'Adyen Test' // Name to be shown
-            // merchantIdentifier: '' // Required in Production environment. Google's merchantId: https://developers.google.com/pay/api/web/guides/test-and-deploy/deploy-production-environment#obtain-your-merchantID
-        },
+    var adyenEnvironment = await httpGet('env', 'ENVIRONMENT');
+    if (adyenEnvironment === "test") {
+      config.googlePayConfig.environment = "TEST";
+    } else if (adyenEnvironment === "live") {
+      config.googlePayConfig.environment = "PRODUCTION";
+    }
 
-        // Events
-        onSubmit: (state, component) => {
-            // Submit Payment
-            makePayment(state.data);
+    config.googlePayConfig.configuration.gatewayMerchantId = await httpGet('env', 'MERCHANT_ACCOUNT');
+    config.googlePayConfig.amount.currency = await httpGet('env', 'CURRENCY');
+    config.googlePayConfig.amount.value = await httpGet('env', 'VALUE');
+    return config;
+};
 
-            updateStateContainer(state); // Demo purposes only
-        },
-        onError: error => {
-            console.error(error);
-        }
-    })
-    // Normally, you should check if Google Pay is available before mounting it.
-    // Here we are mounting it directly for demo purposes.
-    // Please refer to the documentation for more information on Google Pay's availability.
-    .mount('#googlepay-container');
+var googlepayComponent;
+
+let loadComponent = function loadComponent() {
+  getConfig().then(config => {
+
+    var checkout = new AdyenCheckout({
+        environment: config.environment,
+        locale: config.locale
+    });
+
+    // 2. Create and mount the Component
+    googlepayComponent = checkout
+        .create('paywithgoogle', {
+            showPayButton: true,
+            ...config.googlePayConfig,
+
+            // Events
+            onSubmit: (state, component) => {
+                // Submit Payment
+                makePayment(state.data);
+                updateStateContainer(state); // Demo purposes only
+            },
+            onError: error => {
+                console.error(error);
+            }
+        })
+        // Normally, you should check if Google Pay is available before mounting it.
+        // Here we are mounting it directly for demo purposes.
+        // Please refer to the documentation for more information on Google Pay's availability.
+        .mount('#googlepay-container');
+
+      });
+}
+
+loadComponent();
